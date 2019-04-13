7fea8f198dad51bb4f69a8b17779b89437881262
Julian Stobbe <flamel90@gmail.com>
2019-04-13 15:13:46 +0200

    added django

---
+++ photobooth/views.py
@@ -0,0 +1,9 @@
+import time
+
+from django.shortcuts import render
+
+
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
---
+++ pi_installation.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade
+
+# python, pip
+sudo apt-get install python3 -Y
+sudo apt-get install python3-pip -Y
+sudo pip3 install virtualenv virtualenvwrapper
+
+sudo apt-get install git -Y
